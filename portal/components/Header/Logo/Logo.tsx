0937aa698c4c0c32b94bda549c3af66aa9dd5e6e
makhatadze <vitomaxatadze@gmail.com>
2021-06-25 15:28:29 +0400

    Make logo component.

---
+++ portal/components/Header/Logo/Logo.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from './Logo.module.scss';
+import logo from './logo.svg';
+
+
+const Logo = () => {
+    return (
+        <div className={styles.wrapper}>
+            <img className={styles.logo} src={logo} alt="" />
+        </div>
+    )
+}
+
+export default Logo;
